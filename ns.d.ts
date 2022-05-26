47e4c82ce42ab9388f2121020f906346ebd4d245
Dylan <dylanblokhuis3@hotmail.com>
2022-05-26 11:26:38 +0200

    remove jsx h

--- ns.d.ts
+++ ns.d.ts
@@ -1,10 +1,8 @@
-/** @jsx h */
 // deno-lint-ignore-file
 import { type Dust } from "./core/mod.ts";
 import type React from "react"
 
 declare global {
-  var h: typeof React.createElement
   namespace Dust {
     type ActivePlugin = {
       name: string;
